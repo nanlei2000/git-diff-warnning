84e6339a284e6cf712d70c52c8b99512fce684cb
Sushan Yadav <sushanyadav98@gmail.com>
2023-03-04 10:59:14 +0545

    feat: add quick pick to select from the existing branches

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
-import * as vscode from 'vscode';
-import { getCount } from './core';
+import * as vscode from "vscode";
+import { getBranches, getCount } from "./core";
 export function activate() {
+  let selectedBranch: string | undefined = undefined;
+
   const status = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Left,
     9999
@@ -10,7 +12,7 @@ export function activate() {
     return;
   }
   const rootPath = folders[0].uri.path;
-  const pattern = new vscode.RelativePattern(rootPath, '**/*');
+  const pattern = new vscode.RelativePattern(rootPath, "**/*");
   const watcher = vscode.workspace.createFileSystemWatcher(pattern);
   let isNeedWarn: boolean = false;
 
@@ -24,32 +26,58 @@ export function activate() {
 
   const getCountText = (count: number, maxCount: number): string => {
     if (maxCount < count) {
-      return count + '!';
+      return count + "!";
     } else {
-      return count + '';
+      return count + "";
     }
   };
 
   type MaxCountsConfig = [number, number, number, number];
   function getMaxCountFromConfig(): MaxCountsConfig {
-    let config = vscode.workspace.getConfiguration('gitDiffWarning');
+    let config = vscode.workspace.getConfiguration("gitDiffWarning");
     const maxCountEachType = config
-      .get<(number | null)[]>('maxCountEachTypeAndSum')!
-      .map(v => (v !== null ? +v : Infinity));
+      .get<(number | null)[]>("maxCountEachTypeAndSum")!
+      .map((v) => (v !== null ? +v : Infinity));
     return maxCountEachType as MaxCountsConfig;
   }
+
+  // register command to select branch
+  vscode.commands.registerCommand("git-diff.branchSearch", async function () {
+    const branches = await getBranches(rootPath);
+
+    // format branches to quickPickItems
+    const quickPickItems = branches
+      ? branches.map((branch) => {
+          return {
+            label: branch,
+            description: "branch",
+            detail: branch,
+          };
+        })
+      : [];
+
+    const _selectedBranch = await vscode.window.showQuickPick(quickPickItems, {
+      matchOnDetail: true,
+    });
+
+    if (_selectedBranch == null) return;
+
+    selectedBranch = _selectedBranch.label;
+  });
 
   async function updateStatus(): Promise<void> {
     const maxCountEachType = getMaxCountFromConfig();
     const res = await getCount(rootPath);
     if (res) {
-      status.text = ([
-        ['$(file)', res.modifiedFileCount],
-        ['$(diff-added)', res.insertCount],
-        ['$(diff-removed)', res.deleteCount],
-      ] as const)
+      status.text = (
+        [
+          ["$(file)", res.modifiedFileCount],
+          ["$(diff-added)", res.insertCount],
+          ["$(diff-removed)", res.deleteCount],
+        ] as const
+      )
         .map((v, i) => `${v[0]} ${getCountText(v[1], maxCountEachType[i])}`)
-        .join(' ');
+        .join(" ");
       status.tooltip = res.stdout;
       if (
         [
@@ -61,14 +89,14 @@ export function activate() {
       ) {
         if (isNeedWarn === false) {
           vscode.window.showWarningMessage(
-            'WARNING: git diff too big, consider to commit'
+            "WARNING: git diff too big, consider to commit"
           );
         }
         isNeedWarn = true;
-        status.color = '#f00';
+        status.color = "#f00";
       } else {
         isNeedWarn = false;
-        status.color = 'unset';
+        status.color = "unset";
       }
       status.show();
     } else {
